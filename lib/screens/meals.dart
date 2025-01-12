cda561ee0a2d772bc927fa343f044552904237c6
dimon-ton <saichon2namoom@gmail.com>
2025-01-12 19:35:49 +0700

    section 159 Adding Meals Data

---
+++ lib/screens/meals.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, required this.title, required this.meal});
+
+  final String title;
+  final List<Meal> meal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: ,
+    );
+  }
+}
